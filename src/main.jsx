b56876274c88f2e6e6f6e9941cbe9ede03a77ae8
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-02-17 13:45:31 +0600

    set up routes

---
+++ src/main.jsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Home from "./components/Home"
+import AddCoffee from "./components/AddCoffee"
+import UpdateCoffee from "./components/UpdateCoffee"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home></Home>,
+  },
+  {
+    path:"/addCoffee",
+    element:<AddCoffee></AddCoffee>
+  },
+  {
+    path:"/updateCoffee",
+    element:<UpdateCoffee></UpdateCoffee>
+  }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
