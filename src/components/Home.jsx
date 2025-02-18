ef99c74065839579cc4676f9dedeec9b1ad212ad
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-02-18 21:59:15 +0600

    sweet alert show and get data from backend

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
+  const coffees = useLoaderData()
     return (
-      <h1>d3</h1>
+      <div>
+        <h1>Total collection of coffee:{coffees.length}</h1>
+      </div>
     );
 };
 
