a0c457986e23d5073939789e1f82318c32902b53
Rakibul12356 <mdrakibulhasan12346@gmail.com>
2025-02-24 01:36:23 +0600

    update the code

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -3,28 +3,41 @@ import { AuthContext } from './AuthProvider';
 
 
 const SignUp = () => {
-    const {createUser}=useContext(AuthContext)
-    const handleSignUp=event=>{
+    const { createUser } = useContext(AuthContext)
+    const handleSignUp = event => {
         event.preventDefault()
         const form = event.target;
         const email = form.email.value;
         const password = form.password.value;
-        console.log(email,password)
-        createUser(email,password)
-        .then(result=>{
-            console.log(result.user)
-        })
-        .catch(error=>{
-            console.error(error)
-        })
+        console.log(email, password)
+        createUser(email, password)
+            .then(result => {
+                console.log(result.user)
+                //new user has been created
+                const createdAt= result.user?.metadata?.creationTime;
 
+                const user = { email,createdAt:createdAt };
+                fetch('http://localhost:5000/user', {
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(user)
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                    })
+            })
+            .catch(error => {
+                console.error(error)
+            })
     }
     return (
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content flex-col lg:flex-row-reverse">
                 <div className="text-center lg:text-left">
                     <h1 className="text-5xl font-bold">Sign Up now!</h1>
-                   
                 </div>
                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
                     <form onSubmit={handleSignUp} className="card-body">
@@ -32,18 +45,17 @@ const SignUp = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" 
-                            name='email'
-                            placeholder="email" className="input input-bordered" required />
+                            <input type="email"
+                                name='email'
+                                placeholder="email" className="input input-bordered" required />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="password" placeholder="password"
-                            name='password'
-                            className="input input-bordered" required />
-                          
+                                name='password'
+                                className="input input-bordered" required />
                         </div>
                         <div className="form-control mt-6">
                             <button className="btn btn-primary">SignUp</button>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,7 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -16,5 +20,6 @@ export default {
     // eslint-disable-next-line no-undef
     require('daisyui'),
   ],
-}
+};
+
 
